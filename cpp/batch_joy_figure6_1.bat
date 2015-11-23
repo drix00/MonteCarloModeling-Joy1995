63ac82d0d70906e602eddfe6e3295fb97faba557
Hendrix Demers <12611589+drix00@users.noreply.github.com>
2015-11-23 14:55:02 -0500

    Add bactch and input file for chapter 3 code.

---
+++ cpp/batch_joy_figure6_1.bat
@@ -0,0 +1,7 @@
+projects\2015\x64\Release\montecarlomodeling-joy1995.exe < inputs\ag10kV.txt > ag10kV.dat
+projects\2015\x64\Release\montecarlomodeling-joy1995.exe < inputs\al10kV.txt > al10kV.dat
+projects\2015\x64\Release\montecarlomodeling-joy1995.exe < inputs\au10kV.txt > au10kV.dat
+projects\2015\x64\Release\montecarlomodeling-joy1995.exe < inputs\c10kV.txt > c10kV.dat
+projects\2015\x64\Release\montecarlomodeling-joy1995.exe < inputs\cu10kV.txt > cu10kV.dat
+projects\2015\x64\Release\montecarlomodeling-joy1995.exe < inputs\fe10kV.txt > fe10kV.dat
+projects\2015\x64\Release\montecarlomodeling-joy1995.exe < inputs\si10kV.txt > si10kV.dat
